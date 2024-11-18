06017635791bdb02f8094edb9bb84e9e1c8c9863
Ruivo0210 <joaolucasr0210@gmail.com>
2024-11-18 12:32:05 -0300

    finalizando S1 do curso

---
+++ index.js
@@ -0,0 +1,3 @@
+// This is my first JavaScript Code!
+console.log ('Hello World'); // Declaração: pedaço de código que define uma ação a ser executada. Código em JS deve ser terminado com ";". String: sequência de caracteres.
+//JavaScript define o comportamento do documento.
